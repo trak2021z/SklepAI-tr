4b7a78fde9af3e7bc012cf2e9529d4fd57e6c140
Szymon <Szymszello@gmail.com>
2021-06-12 15:38:56 +0200

    - created interface for products repository

---
+++ Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using SklepAI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SklepAI.Interfaces
+{
+    interface IProductRepository
+    {
+        IQueryable<Product> Products { get; }
+        void SaveProduct(Product product);
+        Product DeleteProductByID(int productID);
+    }
+}
