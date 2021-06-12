727406ea0f30c8051c6f3ad85a592bc002102032
Szymon <Szymszello@gmail.com>
2021-06-12 15:39:23 +0200

    - implemented product repository for Entity Framework

---
+++ EFRepositoryControllers/ProductRepository.cs
@@ -0,0 +1,59 @@
+﻿using SklepAI.Data;
+using SklepAI.Interfaces;
+using SklepAI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SklepAI.EFRepositoryControllers
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+        public IQueryable<Product> Products => context.Products;
+
+        public void CreateProduct(Product product)
+        {
+            context.Add(product);
+            context.SaveChanges();
+        }
+
+        public Product DeleteProductByID(int productID)
+        {
+            Product dbEntry = context.Products.FirstOrDefault(p => p.ProductID == productID);
+            if (dbEntry != null)
+            {
+                context.Products.Remove(dbEntry);
+                context.SaveChanges();
+            }
+            return dbEntry;
+        }
+        public void SaveProduct(Product product)
+        {
+            if (product.ProductID == 0)
+            {
+                context.Products.Add(product);
+            }
+            else
+            {
+                Product dbEntry = context.Products.FirstOrDefault(p => p.ProductID == product.ProductID);
+                if (dbEntry != null)
+                {
+                    dbEntry.Name = product.Name;
+                    dbEntry.Description = product.Description;
+                    dbEntry.Price = product.Price;
+                    dbEntry.Category = product.Category;
+                    dbEntry.ImgUrl = product.ImgUrl;
+                    dbEntry.Quantity = product.Quantity;
+                }
+            }
+            context.SaveChanges(); 
+        }
+    }
+}
