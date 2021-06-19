d66079151bcc8f09ec39c2ee195ec207a095e50e
Szymon <Szymszello@gmail.com>
2021-06-19 19:10:17 +0200

    - added pagination to product page

--- Infrastructure/PageLinkTagHelper.cs
+++ Infrastructure/PageLinkTagHelper.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 
 namespace SklepAI.Infrastructure
 {
-    [HtmlTargetElement("div", Attributes = "page-model")]
+    [HtmlTargetElement("ul", Attributes = "page-model")]
     public class PageLinkTagHelper : TagHelper
     {
         private readonly IUrlHelperFactory urlHelperFactory;
@@ -29,26 +29,85 @@ namespace SklepAI.Infrastructure
         public string PageClass { get; set; }
         public string PageClassNormal { get; set; }
         public string PageClassSelected { get; set; }
+        public string PageCount { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+            var pagesCount = int.Parse(PageCount);
             IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
-            TagBuilder result = new TagBuilder("div");
-            for (int i = 1; i <= PageModel.TotalPages; i++)
+            TagBuilder result = new("ul");
+            //previous button <li> a w nim <a> 
+            TagBuilder previousA = new("a");
+            previousA.AddCssClass("page-link");
+            if (PageModel.CurrentPage < 2)
             {
-                TagBuilder tag = new TagBuilder("a");
+                previousA.Attributes["aria-disabled"] = "true";
+                previousA.Attributes["tabindex"] = "-1";
+            }
+            PageUrlValues["productPage"] = PageModel.CurrentPage - 1;
+            previousA.Attributes["href"] = urlHelper.Action(PageAction, PageUrlValues);
+            previousA.InnerHtml.Append("Previous");
+
+            TagBuilder previousLi = new("li");
+            previousLi.AddCssClass(PageClass);
+            if (PageModel.CurrentPage < 2)
+            {
+                previousLi.AddCssClass("disabled");
+            }
+            //zagnieżdżenie a w li 
+            previousLi.InnerHtml.AppendHtml(previousA);
+            result.InnerHtml.AppendHtml(previousLi);
+
+
+            var startPage = PageModel.CurrentPage - 1;
+            if (startPage < 1)
+                startPage = 1;
+            if (startPage + pagesCount > PageModel.TotalPages)
+                startPage = PageModel.TotalPages - pagesCount + 1;
+
+            //trzy w srodku
+            for (int i = startPage; i < startPage + pagesCount; i++)
+            {
                 PageUrlValues["productPage"] = i;
-                tag.Attributes["href"] = urlHelper.Action(PageAction, PageUrlValues);
+                TagBuilder aTag = new("a");
+                aTag.AddCssClass("page-link");
+                aTag.Attributes["href"] = urlHelper.Action(PageAction, PageUrlValues);
+                aTag.InnerHtml.Append(i.ToString());
+                TagBuilder liTag = new("li");
                 if (PageClassesEnabled)
                 {
-                    tag.AddCssClass(PageClass);
-                    tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                    liTag.AddCssClass(PageClass);
+                    liTag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
                 }
-                tag.InnerHtml.Append(i.ToString());
-                result.InnerHtml.AppendHtml(tag);
-            }
-            output.Content.AppendHtml(result.InnerHtml);
+                if (PageModel.CurrentPage == i)
+                    liTag.Attributes["aria-current"] = "page";
 
+                liTag.InnerHtml.AppendHtml(aTag);
+                result.InnerHtml.AppendHtml(liTag);
+            }
+            //next button
+            TagBuilder nextA = new("a");
+            nextA.AddCssClass("page-link");
+            if (PageModel.CurrentPage == PageModel.TotalPages)
+            {
+                nextA.Attributes["aria-disabled"] = "true";
+            }
+            PageUrlValues["productPage"] = PageModel.CurrentPage + 1;
+            nextA.Attributes["href"] = urlHelper.Action(PageAction, PageUrlValues);
+            nextA.InnerHtml.Append("Next");
+
+            TagBuilder nextLi = new("li");
+
+            nextLi.AddCssClass(PageClass);
+            if (PageModel.CurrentPage == PageModel.TotalPages)
+            {
+                nextLi.AddCssClass("disabled");
+            }
+            //zagnieżdżenie a w li 
+            nextLi.InnerHtml.AppendHtml(nextA);
+            result.InnerHtml.AppendHtml(nextLi);
+
+            output.Content.AppendHtml(result.InnerHtml);
         }
     }
 }
